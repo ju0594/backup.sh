1c4b35fe64c0bba424b1a60d9f4141d7bbfc1909
ju0594 <ju0594@126.com>
2020-03-25 12:20:03 +0800

    test

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 #
 
-[[ ! -f /backup/backup.sh ]] && cp /backup.sh /backup/backup.sh
-[[ ! -f /backup/crontabs/root ]] && cp /crontab /backup/crontabs/root
+  
+[[ ! -f /backup/backup.sh ]] && mkdir -p /backup && cp /backup.sh /backup/backup.sh
+[[ ! -f /backup/crontabs/root ]] && mkdir /backup/crontabs && cp /crontab /backup/crontabs/root
 
 crond -f -S -l 0 -c /backup/crontabs
